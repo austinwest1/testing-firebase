5d5e5efed0c744e90041d8b02fe5bfa1a6f49127
austinwest1 <austinwest1@mail.weber.edu>
2022-01-22 16:37:11 -0700

    adding firebase stuff

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-<h1>Firebase Testing</h1>
+<?php
+
+require_once 'GFirestore.php';
+
+use Kreait\Firebase\Factory;
+use Kreait\Firebase\ServiceAccount;
+
+
+$serviceAccount = ServiceAccount::fromJsonFile(__DIR__.'/secret/arthursystems-php-tutorials-0066e2bd5954.json');
+
+$firebase = (new Factory)
+    ->withServiceAccount($serviceAccount)
+    ->create();
+
+$database = $firebase->getDatabase();
+
+die(print_r($database));
